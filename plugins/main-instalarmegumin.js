bebe3e4c64ce32f5a4c52cd419332ac87264a587
Eliasivan <162828351+elias1524@users.noreply.github.com>
2024-08-21 21:35:46 -0400

    Actualizar main-instalarmen.js

--- plugins/main-instalarmegumin.js
+++ plugins/main-instalarmegumin.js
@@ -1,12 +1,1 @@
-//import { generateWAMessageFromContent } from '@adiwajshing/baileys'
-let { generateWAMessageFromContent } = (await import(global.baileys)).default 
-let handler  = async (m, { conn }) => {
-let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
-let texto = `*No seas rata bro, para comprar el bot o saber los precios comunícate con mi creador https://wa.me/595972157130*` 
-let aa = { quoted: m, userJid: conn.user.jid }
-let prep = generateWAMessageFromContent(m.chat, { extendedTextMessage: { text: texto, contextInfo: { externalAdReply: { title: 'Goku-Black-Bot-MD-Lite', body: null, thumbnail: imagen1, sourceUrl: '' }, mentionedJid: [m.sender] }}}, aa)
-conn.relayMessage(m.chat, prep.message, { messageId: prep.key.id, mentions: [m.sender] })  
-}
-handler.command = /^(instalarbot)/i
-handler.register = true
-export default handler
+xddd
