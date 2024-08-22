dd5dbd15bf6fd7a61faf49d3cb803207d3b9131d
Eliasivan <162828351+elias1524@users.noreply.github.com>
2024-08-21 21:15:11 -0400

    Actualizar main-script.js

--- plugins/main-script.js
+++ plugins/main-script.js
@@ -1,28 +1,19 @@
-import moment from 'moment-timezone'
-import fetch from 'node-fetch'
+import { promises } from 'fs'
+import { join } from 'path'
 
-let handler = async (m, { conn, args }) => {
-let res = await fetch('https://github.com/David-Chian/Megumin-Bot-MD')
-let json = await res.json()
-try {
-let txt = `*乂  S C R I P T  -  M A I N  乂*\n\n`
-    txt += `✩  *Nombre* : ${json.name}\n`
-    txt += `✩  *Visitas* : ${json.watchers_count}\n`
-    txt += `✩  *Peso* : ${(json.size / 1024).toFixed(2)} MB\n`
-    txt += `✩  *Actualizado* : ${moment(json.updated_at).format('DD/MM/YY - HH:mm:ss')}\n`
-    txt += `✩  *Url* : ${json.html_url}\n`
-    txt += `✩  *Forks* : ${json.forks_count}\n`
-    txt += `✩  *Stars* : ${json.stargazers_count}\n\n`
-    txt += `> 🍟 *${packname}*`
+let handler = async function (m, { conn, __dirname }) {
+let _package = JSON.parse(await promises.readFile(join(__dirname, '../package.json')).catch(_ => ({}))) || {}
+  
+m.reply(`
+*≡ SCRIPT*
 
-let img = imagen1
+▢ Git : ${_package.homepage}
+`.trim())
+    
+}
 
-await conn.sendMini(m.chat, packname, wm, txt, img, img, redes, fkontak)
-} catch {
-await m.react(error)
-}}
 handler.help = ['script']
-handler.tags = ['main']
-handler.command = ['script', 'sc']
-handler.register = true 
+handler.tags = ['tools']
+handler.command = ['sc', 'git', 'script'] 
+
 export default handler
