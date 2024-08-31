722297089a3a74e4293a76a67a2d432581db8a03
Eliasivan <162828351+elias1524@users.noreply.github.com>
2024-08-30 21:16:44 -0400

    Actualizar Juegos+18.js

--- plugins/Juegos+18.js
+++ plugins/Juegos+18.js
@@ -1,7 +1,7 @@
 //Código creado por elias1524
 
 var handler = async m => 
-
+let foto = './storage/logos/Menu1.jpg'
 conn.reply(m.chat, ` 
 ┏━━━━━━━━━━━━━━━━━━━━┅
 ┇  *「 LINK DE LOS JUEGUITOS +18」*       
