4d5572614f6072c4c4eabe0a13f0af48c1b94e92
Eliasivan <162828351+elias1524@users.noreply.github.com>
2024-09-04 19:31:43 -0400

    Actualizar gc-bienvenidos.js

--- plugins/gc-bienvenidos.js
+++ plugins/gc-bienvenidos.js
@@ -9,7 +9,7 @@ let handler = async (m, { conn }) => {
         },
         message: {
             videoMessage: { 
-                title: 'Megumin', 
+                title: 'GokuBlack', 
                 h: `Hmm`,
                 seconds: '99999', 
                 gifPlayback: true, 
