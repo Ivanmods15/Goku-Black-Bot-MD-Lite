8f2a55a9e76cf85a851d9883dcbadf58c0898896
Eliasivan <162828351+elias1524@users.noreply.github.com>
2024-09-04 19:19:18 -0400

    🪼GOKU-BLACK-BOT-MD-LITE🪼
    ✨Goku-Black-Bot-MD-Lite✨

--- plugins/mods-unbanchat.js
+++ plugins/mods-unbanchat.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn }) => {
 if (!(m.chat in global.db.data.chats)) return conn.reply(m.chat, '🍭 *¡ESTE CHAT NO ESTÁ REGISTRADO!*', m, fake)
 let chat = global.db.data.chats[m.chat]
-if (!chat.isBanned) return conn.reply(m.chat, '🍟 *¡Gogeta-Bot NO ESTÁ BANEADO EN ESTE CHAT!*', m, fake)
+if (!chat.isBanned) return conn.reply(m.chat, '🍟 *¡GokuBlack-Bot NO ESTÁ BANEADO EN ESTE CHAT!*', m, fake)
 chat.isBanned = false
-await conn.reply(m.chat, '🚩 *¡Gogeta-Bot YA FUÉ DESBANEADO EN ESTE CHAT!*', m, fake)
+await conn.reply(m.chat, '🚩 *¡GokuBlack-Bot YA FUÉ DESBANEADO EN ESTE CHAT!*', m, fake)
 }
 handler.help = ['unbanchat'];
 handler.tags = ['mods'];
