911ec3d65c3082616d026da57348632a6c216691
Eliasivan <162828351+elias1524@users.noreply.github.com>
2024-09-06 15:38:13 -0400

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -3,19 +3,85 @@ import fetch from 'node-fetch'
 
 export async function before(m, {conn, participants, groupMetadata}) {
   if (!m.messageStubType || !m.isGroup) return !0;
+    let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => welcome)
+    let pp2 = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => adios)
+  let img = await (await fetch(`${pp}`)).buffer()
+  let img2 = await (await fetch(`${pp2}`)).buffer()
+
   let chat = global.db.data.chats[m.chat]
 
   if (chat.welcome && m.messageStubType == 27) {
-    let bienvenido = `Bienvenido @${m.messageStubParameters[0].split`@`[0]}`
-await conn.sendMini(m.chat, packname, dev, bienvenido, welcome, welcome, channel, fkontak)
+    let wel = ` ┌─★ GokuBlack-Bot-MD ⚡ \n │「 𝗕𝗶𝗲𝗻𝘃𝗲𝗻𝗶𝗱𝗼 ☁ 」\n └┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✨  𝗕𝗶𝗲𝗻𝘃𝗲𝗻𝗶𝗱𝗼/𝗮\n   │✨  ${groupMetadata.subject}\n   └───────────────┈ ⳹`
+await conn.sendMini(m.chat, packname, dev, wel, img, img, channel, fkontak)
   }
 
   if (chat.welcome && m.messageStubType == 28) {
-    let bye = `Adios @${m.messageStubParameters[0].split`@`[0]}`
-await conn.sendMini(m.chat, packname, dev, bye, adios, adios, channel, fkontak)
+   let bye = ` ┌─★ GokuBlack-Bot-MD ⚡ \n │「 𝗔𝗗𝗜𝗢𝗦 🌪️ 」\n └┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✨  𝗦𝗲 𝗳𝘂𝗲\n   │✨ 𝗡𝘂𝗻𝗰𝗮 𝘁𝗲 𝗾𝘂𝗶𝘀𝗶𝗺𝗼𝘀 𝗮𝗾𝘂𝗶\n   └───────────────┈ ⳹`
+await conn.sendMini(m.chat, packname, dev, bye, img2, img2, channel, fkontak)
   }
 
   if (chat.welcome && m.messageStubType == 32) {
-    let kick = `Adios @${m.messageStubParameters[0].split`@`[0]}`
-await conn.sendMini(m.chat, packname, dev, kick, adios, adios, channel, fkontak)
+    let kick = ` ┌─★ GokuBlack-Bot-MD ⚡\n │「 𝗔𝗗𝗜𝗢𝗦 🌪️ 」\n └┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✨  𝗦𝗲 𝗳𝘂𝗲\n   │✨ 𝗡𝘂𝗻𝗰𝗮 𝘁𝗲 𝗾𝘂𝗶𝘀𝗶𝗺𝗼𝘀 𝗮𝗾𝘂𝗶\n   └───────────────┈ ⳹`
+await conn.sendMini(m.chat, packname, dev, kick, img2, img2, channel, fkontak)
 }}
+
+/*import { WAMessageStubType } from '@whiskeysockets/baileys';
+import fetch from 'node-fetch';
+
+export async function before(m, { conn, participants, groupMetadata }) {
+  if (!m.messageStubType || !m.isGroup) return true;
+
+  let vn = 'https://qu.ax/cUYg.mp3';
+  let vn2 = 'https://qu.ax/cTDa.mp3';
+  let welc = welcome;
+  let adi = adios;
+  let chat = global.db.data.chats[m.chat];
+  const getMentionedJid = () => {
+    return m.messageStubParameters.map(param => `${param}@s.whatsapp.net`);
+  };
+
+  let who = m.messageStubParameters[0] + '@s.whatsapp.net';
+  let user = global.db.data.users[who];
+
+  let userName = user ? user.name : await conn.getName(who);
+
+  if (chat.welcome && m.messageStubType === 27) {
+    this.sendMessage(m.chat, {
+      audio: { url: vn },
+      contextInfo: {
+        mentionedJid: getMentionedJid(),
+        "externalAdReply": {
+          "thumbnail": welc,
+          "title": "  ͟͞ Ｗ Ｅ Ｌ Ｃ Ｏ Ｍ Ｅ ͟͞  ",
+          "body": `${userName}!`,
+          "previewType": "PHOTO",
+          "thumbnailUrl": null,
+          "showAdAttribution": true,
+          sourceUrl: [yt, md, channel].sort(() => 0.5 - Math.random())[0]
+        }
+      },
+      ptt: true,
+      mimetype: 'audio/mpeg',
+      fileName: 'welcome.mp3'
+    }, { quoted: fkontak });
+  }
+
+  if (chat.welcome && (m.messageStubType === 28 || m.messageStubType === 32)) {
+    this.sendMessage(m.chat, {
+      audio: { url: vn2 },
+      contextInfo: {
+        mentionedJid: getMentionedJid(),
+        "externalAdReply": {
+          "showAdAttribution": true,
+          "containsAutoReply": true,
+          "title": '  ͟͞ Ａ Ｄ Ｉ Ｏ́ Ｓ ͟͞  ',
+          body: `${userName}, se despide.`,
+          "previewType": "PHOTO",
+          "thumbnailUrl": '',
+          "thumbnail": adi,
+          "sourceUrl": redes
+        }
+      }
+    }, { quoted: fkontak });
+  }
+}*/
