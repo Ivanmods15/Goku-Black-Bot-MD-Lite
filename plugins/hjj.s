3856a5d50104245273f59189c170781609e35d0e
Eliasivan <162828351+elias1524@users.noreply.github.com>
2024-09-10 21:31:06 -0400

    Actualizar hjj.s

--- plugins/hjj.s
+++ plugins/hjj.s
@@ -1,7 +1,1 @@
-const handler = async (m, {conn, command}) => {
-  console.log(Ivan);
-  new Error('This is an error');
-};
-handler.command = /^(omg)$/i;
-handler.owner = true;
-export default handler;
+.
