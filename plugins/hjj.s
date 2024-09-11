dd0b3a7418387f86c97a47ae27ebb1f80ed08974
Eliasivan <162828351+elias1524@users.noreply.github.com>
2024-09-10 21:40:48 -0400

    Actualizar hjj.s

--- plugins/hjj.s
+++ plugins/hjj.s
@@ -1,11 +1,49 @@
-import fetch from 'node-fetch';
-const handler = async (m, {conn, args, text}) => {
-  if (!text) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙳𝙴 𝚄𝙽 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙳𝙴 𝚃𝙸𝙺𝚃𝙾𝙺*';
-  const res = `https://api.lolhuman.xyz/api/pptiktok/${text}?apikey=${lolkeysapi}`;
-  conn.sendFile(m.chat, res, 'error.jpg', `*[ ✔ ] 𝙰𝚀𝚄𝙸 𝙴𝚂𝚃𝙰 𝙻𝙰 𝙵𝙾𝚃𝙾 𝙳𝙴 𝙿𝙴𝚁𝙵𝙸𝙻 𝙳𝙴 ${text}*`, m, false);
-};
-handler.help = ['tiktokfoto'].map((v) => v + ' <username>');
-handler.tags = ['downloader'];
-handler.command = /^(tiktokfoto|pptiktok)$/i;
-export default handler;
-.
+import fg from 'api-dylux'
+import fetch from 'node-fetch'
+let handler = async (m, { conn, text, args, usedPrefix, command }) => {
+    
+        if (!args[0]) throw `✳️ ${mssg.noLink('TikTok')}\n\n 📌 ${mssg.example} : ${usedPrefix + command} https://vm.tiktok.com/ZMYG92bUh/`
+        if (!args[0].match(/tiktok/gi)) throw `❎ ${mssg.noLink('TikTok')}`
+        m.react(rwait)
+      
+        try {
+        let res = await fetch(global.API('fgmods', '/api/downloader/tiktok', { url: args[0] }, 'apikey'))
+        let data = await res.json()
+
+        if (!data.result.images) {
+            let tex = `
+┌─⊷ *TIKTOK DL* 
+▢ *${mssg.name}:* ${data.result.author.nickname}
+▢ *${mssg.username}:* ${data.result.author.unique_id}
+▢ *${mssg.duration}:* ${data.result.duration}
+▢ *Likes:* ${data.result.digg_count}
+▢ *${mssg.views}:* ${data.result.play_count}
+▢ *${mssg.desc}:* ${data.result.title}
+└───────────
+`
+            conn.sendFile(m.chat, data.result.play, 'tiktok.mp4', tex, m);
+            m.react(done)
+        } else {
+            let cap = `
+▢ *Likes:* ${data.result.digg_count}
+▢ *${mssg.desc}:* ${data.result.title}
+`
+            for (let ttdl of data.result.images) {
+                conn.sendMessage(m.chat, { image: { url: ttdl }, caption: cap }, { quoted: m })
+            }
+            conn.sendFile(m.chat, data.result.play, 'tiktok.mp3', '', m, null, { mimetype: 'audio/mp4' })
+            m.react(done)
+        }
+
+      } catch (error) {
+        m.reply(`❎ ${mssg.error}`)
+    }
+   
+}
+
+handler.help = ['tiktok']
+handler.tags = ['dl']
+handler.command = ['tiktok', 'tt', 'tiktokimg', 'tiktokslide']
+handler.diamond = true
+
+export default handler
