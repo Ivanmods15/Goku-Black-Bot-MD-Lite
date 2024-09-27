98671404c3d53a6332e1ebde14316503c04eb0e4
Eliasivan <162828351+elias1524@users.noreply.github.com>
2024-09-27 16:58:48 -0400

    GokuBlack

--- plugins/_tutorial_reg.js
+++ plugins/_tutorial_reg.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn }) => {
   const usuario = m.pushName || 'Usuario';
   const videoUrl = 'https://telegra.ph/file/330a7838421e8f555b7be.mp4';
 
-  const texto = `Hola @${m.sender.split('@')[0]} aquí está el tutorial para registrarte en Megumin-Bot.`;
+  const texto = `Hola @${m.sender.split('@')[0]} SOMOS TEAM FELIX ROJAS GRACIAS POR UTILIZAR ESTE COMANDO❤️`;
 
   const options = {
     quoted: m,
@@ -13,6 +13,6 @@ let handler = async (m, { conn }) => {
   await conn.sendMessage(m.chat, { video: { url: videoUrl }, ...options });
 };
 
-handler.command = ['Priv']
+handler.command = ['Felix']
 
 export default handler;
