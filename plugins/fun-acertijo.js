8ee2e1414ee30172ee59264daff12fe9f1a1868e
Eliasivan <162828351+elias1524@users.noreply.github.com>
2024-10-10 15:38:19 -0300

    Goku-Black-Bot-MD-Lite 💥

--- plugins/fun-acertijo.js
+++ plugins/fun-acertijo.js
@@ -1,34 +1,40 @@
-import fs from 'fs';
-const timeout = 60000;
-const poin = 10;
-const handler = async (m, {conn, usedPrefix}) => {
-  conn.tekateki = conn.tekateki ? conn.tekateki : {};
-  const id = m.chat;
-  if (id in conn.tekateki) {
-    conn.reply(m.chat, 'Todavía hay acertijos sin responder en este chat', conn.tekateki[id][0]);
-    throw false;
-  }
-  const tekateki = JSON.parse(fs.readFileSync(`./src/game/acertijo.json`));
-  const json = tekateki[Math.floor(Math.random() * tekateki.length)];
-  const _clue = json.response;
-  const clue = _clue.replace(/[A-Za-z]/g, '_');
-  const caption = `
-ⷮ🚩 *ACERTIJOS*
-✨️ *${json.question}*
+import fs from 'fs'
 
-⏱️ *Tiempo:* ${(timeout / 1000).toFixed(2)} Segundos
-🎁 *Premio:* *+${poin}* Centavos 🪙`.trim();
-  conn.tekateki[id] = [
-    await conn.reply(m.chat, caption, m), json,
-    poin,
-    setTimeout(async () => {
-      if (conn.tekateki[id]) await conn.reply(m.chat, `🤍 Se acabó el tiempo!\n*Respuesta:* ${json.response}`, conn.tekateki[id][0]);
-      delete conn.tekateki[id];
-    }, timeout)];
-};
-handler.help = ['acertijo'];
-handler.tags = ['fun'];
-handler.group = true;
+let timeout = 60000
+let poin = 500
+
+var handler = async (m, { conn, usedPrefix }) => {
+
+conn.tekateki = conn.tekateki ? conn.tekateki : {}
+let id = m.chat
+if (id in conn.tekateki) {
+conn.reply(m.chat, '🚩 *Hay acertijos sin responder en este chat*', conn.tekateki[id][0], fake, )
+throw false
+}
+let tekateki = JSON.parse(fs.readFileSync(`./storage/juegos/acertijo.json`))
+let json = tekateki[Math.floor(Math.random() * tekateki.length)]
+let _clue = json.response
+let clue = _clue.replace(/[A-Za-z]/g, '_')
+let caption = `
+ⷮ${json.question}
+
+*• ⏰ Tiempo:* ${(timeout / 1000).toFixed(2)} segundos
+*• 🎁 Bono:* +${poin} Exp
+`.trim()
+conn.tekateki[id] = [
+await conn.reply(m.chat, caption, m),
+json, poin,
+setTimeout(async () => {
+if (conn.tekateki[id]) await conn.reply(m.chat, `🚩 *Se terminó el tiempo*`, conn.tekateki[id][0], fake, )
+delete conn.tekateki[id]
+}, timeout)
+]
+}
+
+handler.help = ['acertijo', 'acert', 'adivinanza', 'tekateki']
+handler.tags = ['juegos']
+handler.command = /^(acertijo|acert|adivinanza|tekateki)$/i
+
 handler.register = true
-handler.command = ['acertijo','acert','adivinanza','tekateki']
-export default handler;
+
+export default handler
