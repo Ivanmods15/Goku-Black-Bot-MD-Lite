2ef65b1416b9572561e23e1420943083c8427e0e
Eliasivan <162828351+elias1524@users.noreply.github.com>
2024-10-10 18:07:15 -0300

    Goku-Black-Bot-MD-Lite 💥

---
+++ plugins/info-donar.js
@@ -0,0 +1,13 @@
+import {generateWAMessageFromContent} from '@whiskeysockets/baileys';
+import fs from 'fs';
+const handler = async (m, {conn, usedPrefix, command}) => {
+  const name = await conn.getName(m.sender);
+  const donar =`*TE AGRADECERIA MUCHO SI ME DEJAS UNA ESTRELLITA EN EL REPO*`.trim();
+  const aa = {quoted: m, userJid: conn.user.jid};
+  const res = generateWAMessageFromContent(m.chat, {liveLocationMessage: {degreesLatitude: 0, degreesLongitude: 0, caption: donar, secuenceNumber: '0', contextInfo: {mentionedJid: conn.parseMention()}}}, aa);
+  conn.relayMessage(m.chat, res.message, {});
+};
+handler.help = ['Estrella'];
+handler.tags = ['estrellita'];
+handler.command = /^estre(estrella|estrellita)|donar|apoyar$/i;
+export default handler;
