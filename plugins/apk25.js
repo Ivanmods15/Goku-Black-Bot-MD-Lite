b4fc033dc1c48100777f3763bda44508fc1cd4aa
Eliasivan <162828351+elias1524@users.noreply.github.com>
2024-10-18 16:42:13 -0300

    Actualizar apk25.js

--- plugins/apk25.js
+++ plugins/apk25.js
@@ -1,4 +1,4 @@
-import Starlights from "@StarlightsTeam/Scraper"
+import { search, download } from '@whiskeysockets/baileys'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 if (!text) return conn.reply(m.chat, `*• Ingresa el nombre de la aplicación que deseas descargar.*\n\nEjemplo:\n*${usedPrefix + command}* WhatsApp`, m, rcanal)
